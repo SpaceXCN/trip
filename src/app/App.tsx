ced9f729eb1d34b8b10ad0b647145f910c73cee8
mxsm <mxsm@apache.org>
2026-05-24 13:38:25 +0800

    Add Vercel Analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 import { router } from "./routes";
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+      <Analytics />
+    </>
   );
 }
